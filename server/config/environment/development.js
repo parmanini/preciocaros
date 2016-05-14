8889914d7fdabce2219e1c9d5e1e54530a52fe60
Pablo Armanini <pol@Pablos-MBP.valhalla>
2016-05-14 14:53:01 -0300

    Primer commit con lo basico

---
+++ server/config/environment/development.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/precioscaros-dev'
+  },
+
+  // Seed database on startup
+  seedDB: true
+
+};
